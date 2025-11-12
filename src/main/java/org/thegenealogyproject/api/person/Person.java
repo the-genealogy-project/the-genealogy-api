623e24946beb0d36b573f91a160a6fd1a54edd67
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2025-11-12 16:14:39 +0100

    Fix Person

--- src/main/java/org/thegenealogyproject/api/person/Person.java
+++ src/main/java/org/thegenealogyproject/api/person/Person.java
@@ -1,6 +1,7 @@
 package org.thegenealogyproject.api.person;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
@@ -18,10 +19,12 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Person {
 
     @Id
     @GeneratedValue
+    @EqualsAndHashCode.Include
     private UUID id;
 
     private String firstName;
@@ -38,16 +41,16 @@ public class Person {
 
     private LocalDate dateOfDeath;
 
-    @Relationship("")
-    private Set<Person> parents = new HashSet<>();
-
-    @Relationship("")
+    @Relationship(type = "PARENT_OF", direction = Relationship.Direction.OUTGOING)
     private Set<Person> children = new HashSet<>();
 
-    @Relationship("")
+    @Relationship(type = "PARENT_OF", direction = Relationship.Direction.INCOMING)
+    private Set<Person> parents = new HashSet<>();
+
+    @Relationship(type = "SIBLING_OF", direction = Relationship.Direction.OUTGOING)
     private Set<Person> siblings = new HashSet<>();
 
-    @Relationship("")
+    @Relationship(type = "SPOUSE_OF", direction = Relationship.Direction.OUTGOING)
     private Set<Person> spouses = new HashSet<>();
 
     public int calculateAge() {
